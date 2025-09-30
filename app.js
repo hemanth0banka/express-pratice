9b2f4aaa3b878c9b495ff0731ed61734cff4f49a
hemanth0banka <150692843+hemanth0banka@users.noreply.github.com>
2025-09-30 19:29:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const express = require("express");
+const express = require("express")
 const app = express()
-const port = 1005
+const port = 3000
 
 app.use(express.json())
 
-app.use((req,res,next)=>{
-    req.user = "guest"
-    next()
-})
+let users = [{id : 1,name : "Hemanth"}];
+let orders = [{id : 1,name : "phone"}];
 
-app.get("/welcome",(req,res)=>{
-    res.send(`<h1>Welcome, ${req.user}!</h1>`)
+app.get("/orders",(req,res)=>{
+    res.status(200).json(orders).send(`Here is the List Of All Orders ${orders}`)
+})
+app.get("/users",(req,res)=>{
+    res.status(200).json(users).send(`Here is the List Of All Users ${users}`)
+})
+app.post("/orders",(req,res)=>{
+    let {name} = req.body
+    let obj = {id : orders.length+1 , name}
+    orders.push(obj)
+    res.status(201).json(obj).send(`Here is the List Of All Orders ${orders}`)
+})
+app.post("/users",(req,res)=>{
+    let {name} = req.body
+    let obj = {id : users.length+1 , name}
+    users.push(obj)
+    res.status(201).json(obj).send(`Here is the List Of All Orders ${users}`)
 })
 
 app.listen(port,()=>{
-    console.log(`listening at port ${port}`)
+    console.log(`Listening at port ${port}`)
 })
